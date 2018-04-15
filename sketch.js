395381518726e88e035fbfcb81b9b7191341648a
Andrew Castillo <valax321@gmail.com>
2018-04-15 22:40:15 +1000

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+
+// Any object that can interact with mouse clicks (hover, mouse down, mouse up etc.)
+class Interactable
+{
+    constructor()
+    {
+        this.enabled = true;
+        this.position = createVector(0, 0);
+    }
+
+    // Is the mouse currently over this interactable?
+    isHovered()
+    {
+        //Use p5.collide
+    }
+}
+
+
+
+function preload()
+{
+
+}
+
+function setup()
+{
+
+}
+
+function draw()
+{
+
+}
