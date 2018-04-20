7dd5eccede4978bf3367e9269e97c01bb46505bc
Andrew Castillo <valax321@gmail.com>
2018-04-20 14:14:34 +1000

    Howitzer aiming and screams

--- sketch.js
+++ sketch.js
@@ -4,6 +4,42 @@ var resolutionScale = 1;
 
 var interactables = [];
 
+function deltaTime()
+{
+    return 1 / getFrameRate();
+}
+
+//It would be nice if I didn't have to keep writing my own maths functions...
+
+// Float is approximately equal to another float
+function approx(a, b, delta = 0.001)
+{
+    var diff = abs(a - b);
+    return diff <= delta;
+}
+
+function clamp(value, a, b)
+{
+    if (a > b)
+    {
+        var temp = b;
+        b = a;
+        a = temp;
+    }
+    if (value > b) return b;
+    if (value < a) return a;
+    return value;
+}
+
+// https://answers.unity.com/questions/227736/clamping-a-wrapping-rotation.html
+function clampAngle(angle, from, to)
+{
+    if (angle > 180) angle = 360 - angle;
+    angle = clamp(angle, from, to);
+    if (angle < 0) angle = 360 + angle;
+    return angle;
+}
+
 // Any object that can interact with mouse clicks (hover, mouse down, mouse up etc.)
 class Interactable
 {
@@ -32,6 +68,10 @@ class Interactable
     }
 }
 
+const FS_YAW = 0;
+const FS_PITCH = 1;
+const FS_FIRING = 3;
+
 class Howitzer extends Interactable
 {
     constructor()
@@ -43,8 +83,20 @@ class Howitzer extends Interactable
 
         this.angle = 20; //Idle angle
         this.yaw = 0;
+        this.desiredYaw = 0;
+        this.maxYaw = 40;
         this.firing = false;
 
+        this.fireStage = FS_YAW;
+
+        this.maxPitchDistance = 300;
+
+        this.rotateSpeed = 10;
+
+        this.fireTimer = 1;
+        this.currentFireTime = 0;
+
+        //TODO: ADD MORE STATES TO THIS! Probably every 10 degrees
         this.sprites[0][0] = loadImage("assets/howitzer/h_5_normal.png");
         this.sprites[0][1] = loadImage("assets/howitzer/h_20_normal.png");
         this.sprites[0][2] = loadImage("assets/howitzer/h_35_normal.png");
@@ -56,6 +108,8 @@ class Howitzer extends Interactable
         this.sprites[1][3] = loadImage("assets/howitzer/h_60_firing.png");
 
         this.shadowSprite = loadImage("assets/howitzer/h_shadow.png");
+
+        this.aimFont = loadFont("assets/fonts/icbmss25.ttf");
     }
 
     getFiringSpriteIndex()
@@ -66,18 +120,81 @@ class Howitzer extends Interactable
         else return 3;
     }
 
+    mousePressed()
+    {
+        if (this.fireStage == FS_YAW) this.fireStage = FS_PITCH;
+        else if (this.fireStage == FS_PITCH)
+        {
+             this.firing = true;
+             this.fireStage = FS_FIRING;
+        }
+    }
+
     update()
     {
         super.update();
+
+        var mXo = mouseX - this.position.x * resolutionScale; //Because mouse position is in unscaled units.
+        var mYo = mouseY - this.position.y * resolutionScale;
+
+        var mo = createVector(mXo, mYo);
+
+        if (this.fireStage == FS_YAW)
+        {
+            if (getFrameRate() != 0) //HACK: p5 is so well made that getFrameRate() returns 0 on the first frame, meaning deltaTime() returns Infinity. It broke rotation by instantly setting yaw to Infinity too.
+            {
+                this.desiredYaw = clampAngle(degrees(mo.heading()) + 90, this.maxYaw, -this.maxYaw);
+                // Move the howitzer slowly.
+                var dyC = this.desiredYaw > 180 ? (360 - this.desiredYaw) * -1 : this.desiredYaw;
+                var yC = this.yaw > 180 ? 360 - this.yaw : this.yaw;
+                var dir = Math.sign(dyC - yC);
+                if (approx(dyC, yC, 0.1)) dir = 0; //Stop moving if we're pretty close.
+                this.yaw += this.rotateSpeed * dir * deltaTime()
+            }
+        }
+        else if (this.fireStage == FS_PITCH)
+        {
+            var mDist = mo.mag() / resolutionScale;
+            var distScale = pow(clamp(mDist / this.maxPitchDistance, 0, 1), 2);
+            this.angle = lerp(5, 60, distScale);
+        }
+        else if (this.fireStage == FS_FIRING)
+        {
+            this.currentFireTime += deltaTime();
+
+            if (this.currentFireTime > 0.2)
+            {
+                this.firing = false;
+            }
+
+            if (this.currentFireTime >= this.fireTimer)
+            {
+                this.fireStage = FS_YAW;
+                this.currentFireTime = 0;
+            }
+        }
+        else
+        {
+            console.log("Invalid fire stage on howitzer!");
+        }
     }
 
     draw()
     {
         push();
-        translate(this.position.x, this.position.y);
-        rotate(radians(this.yaw));
-        image(this.shadowSprite, 0, 0);
-        image(this.sprites[this.firing | 0][this.getFiringSpriteIndex()], 0, 0);
+            translate(this.position.x, this.position.y);
+            rotate(radians(this.yaw));
+            image(this.shadowSprite, 0, 0);
+            image(this.sprites[this.firing | 0][this.getFiringSpriteIndex()], 0, 0);
+        pop();
+        push();
+            textFont(this.aimFont);
+            fill(255);
+            strokeWeight(1);
+            stroke(0);
+            var mx = (mouseX / resolutionScale) + 10;
+            text("Yaw: " + this.yaw.toFixed(0), mx, mouseY / resolutionScale);
+            text("Pitch: " + this.angle.toFixed(0), mx, (mouseY / resolutionScale) - 15);
         pop();
     }
 }
@@ -105,7 +222,7 @@ function setup()
     imageMode(CENTER);
     var canvas = createCanvas(CANVAS_SIZE.x * resolutionScale, CANVAS_SIZE.y * resolutionScale).elt;
     var ctx = canvas.getContext('2d');
-    ctx.imageSmoothingEnabled = false;
+    //ctx.imageSmoothingEnabled = false;
 }
 
 function windowResized()
@@ -123,6 +240,15 @@ function draw()
     imageMode(CORNER);
     image(bg, 0, 0);
     pop();
+    howy.update();
     howy.draw();
     pop();
+}
+
+function mousePressed()
+{
+    if (howy != null)
+    {
+        howy.mousePressed();
+    }
 }
